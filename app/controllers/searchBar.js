7336e1349b8e6b5cec8ad3b667374850df24dc9a
ayab257 <ayab257@gmail.com>
2016-10-28 09:53:12 +0300

    adding jQuery

--- app/controllers/searchBar.js
+++ app/controllers/searchBar.js
@@ -3,6 +3,31 @@
 	app.controller("searchBar",searchBar);
 	
 	function searchBar($scope, $http) {
+	var key ="&key=AIzaSyBgzihkXKhmcw1jcbKGlOmDi3TpOak1Fm4";
+	var url="https://www.googleapis.com/books/v1/volumes?q=";
+	var QUERY = "+intitle:";
+		$scope.getBooks = function() {
+
+			if ($scope.searchName != '') {
+				var promise = $http.get(url + $scope.search + QUERY+$scope.search  + key);
+					promise.then(successCallback, errorCallback);
+			} else {
+
+				$scope.results = [{
+					name : "no results!"
+				}];
+
+			}
+			function successCallback(response) {
+				$scope.results = response.data.items;
+				console.log("success", response.data.items);
+			}
+
+			function errorCallback(response) {
+				console.log("error", response.data.items);
+			}
+
+		};
 	
 	}
 })();
---
+++ jQuery.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $("#btn1").click(function(){
+        $("#div").fadeOut();
+    });
+});
+
+$(document).ready(function(){
+    $("#btn2").click(function(){
+        $("#div").fadeIn();
+    });
+});
